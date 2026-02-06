698d9244539beb583b16802363cc9285d2dbf02a
Rahul Mahto <rahulmahto2486@gmail.com>
2026-02-06 23:13:19 +0530

    update card

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -5,7 +5,7 @@ import Home from './pages/Home/home'
 import PlaceOrder from './pages/placeorder/placeorder'
 import Footer from './component/Footer/footer'
 import LoginPopup from './component/LoginPopup/LoginPopup'
-import Cart from './pages/cart/cart'
+import Cart from './pages/Cart/Card'
 import Verify from './pages/verify/verify'
 import MyOrder from './pages/MyOrder/MyOrder'
 
